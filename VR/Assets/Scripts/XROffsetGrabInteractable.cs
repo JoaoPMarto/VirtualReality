deda621fabc3e7fc80516725b8977a355eb68913
JoaoPMarto <joaopomarto@tecnico.ulisboa.pt>
2022-12-13 22:50:40 +0000

    added highlight

--- VR/Assets/Scripts/XROffsetGrabInteractable.cs
+++ VR/Assets/Scripts/XROffsetGrabInteractable.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 public class XROffsetGrabInteractable : XRGrabInteractable
 {
+
+    private Vector3 initialLocalPos;
+    private Quaternion initialLocalRot;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject attachPoint = new GameObject("Offset Grab Pivot");
-        attachPoint.transform.SetParent(transform, false);
-        attachTransform = attachPoint.transform;
+        if (!attachTransform)
+        {
+            GameObject attachPoint = new GameObject("Offset Grab Pivot");
+            attachPoint.transform.SetParent(transform, false);
+            attachTransform = attachPoint.transform;
+        }
+        else
+        {
+            initialLocalPos = attachTransform.localPosition;
+            initialLocalRot = attachTransform.localRotation;
+        }
     }
 
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
-        attachTransform.position = args.interactableObject.transform.position;
-        attachTransform.rotation = args.interactableObject.transform.rotation;
+        if(args.interactableObject is XRDirectInteractor)
+        {
+            attachTransform.position = args.interactableObject.transform.position;
+            attachTransform.rotation = args.interactableObject.transform.rotation;
+        }
+        else
+        {
+            attachTransform.localPosition = initialLocalPos;
+            attachTransform.localRotation = initialLocalRot;
+        }
         base.OnSelectEntered(args);
     }
 }
