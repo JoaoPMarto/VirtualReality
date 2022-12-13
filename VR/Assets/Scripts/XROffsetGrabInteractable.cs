a27fbbdb22aee13c3d544535867a4cf117f8dfde
JoaoPMarto <joaopomarto@tecnico.ulisboa.pt>
2022-12-13 22:22:09 +0000

    added distance grabbing

---
+++ VR/Assets/Scripts/XROffsetGrabInteractable.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+public class XROffsetGrabInteractable : XRGrabInteractable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject attachPoint = new GameObject("Offset Grab Pivot");
+        attachPoint.transform.SetParent(transform, false);
+        attachTransform = attachPoint.transform;
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        attachTransform.position = args.interactableObject.transform.position;
+        attachTransform.rotation = args.interactableObject.transform.rotation;
+        base.OnSelectEntered(args);
+    }
+}
