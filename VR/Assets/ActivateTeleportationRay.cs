bbcab143a9a06b59a2ff27fd931e38206aa52fa9
AlexTorrinha <56686370+AlexTorrinha@users.noreply.github.com>
2022-12-15 20:28:38 +0000

    teleport bug fix

--- VR/Assets/ActivateTeleportationRay.cs
+++ VR/Assets/ActivateTeleportationRay.cs
@@ -14,12 +14,6 @@ public class ActivateTeleportationRay : MonoBehaviour
     public InputActionProperty leftActivate;
     public InputActionProperty rightActivate;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
