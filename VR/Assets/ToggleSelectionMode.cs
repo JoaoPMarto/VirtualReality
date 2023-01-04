c9d4dd641503fbfca246419b2f60bcf93569030d
ist1102174 <joaopomarto@tecnico.ulisboa.pt>
2023-01-04 15:41:25 +0000

    added script to switch between selection method

---
+++ VR/Assets/ToggleSelectionMode.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+public class ToggleSelectionMode : MonoBehaviour
+{
+    public InputActionReference toggleReference = null;
+
+    public GameObject rightHand;
+    public GameObject bubble;
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggleReference.action.started += toggleBubble;
+    }
+
+    private void toggleBubble(InputAction.CallbackContext context)
+    {
+        rightHand.SetActive(!rightHand.activeSelf);
+        bubble.SetActive(!bubble.activeSelf);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        toggleReference.action.started -= toggleBubble;
+    }
+}
