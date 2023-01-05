facc8a208aa204307b6d7823331bbabecd4351c2
JoaoPMarto <joaopomarto@tecnico.ulisboa.pt>
2023-01-05 03:20:13 +0000

    bug fix switch between normal and bubble selection mode

--- VR/Assets/ToggleSelectionMode.cs
+++ VR/Assets/ToggleSelectionMode.cs
@@ -6,23 +6,41 @@ using UnityEngine.InputSystem;
 
 public class ToggleSelectionMode : MonoBehaviour
 {
-    public InputActionReference toggleReference = null;
+    public InputActionReference toggleReference;
 
     public GameObject rightHand;
+    public GameObject rightHandModel;
     public GameObject bubble;
+    public GameObject bubbleModel;
+    public GameObject cylinder;
+
     // Start is called before the first frame update
     void Start()
     {
         rightHand.SetActive(true);
+        rightHandModel.SetActive(true);
         bubble.SetActive(false);
+        bubbleModel.SetActive(false);
         toggleReference.action.started += toggleBubble;
     }
 
     private void toggleBubble(InputAction.CallbackContext context)
     {
+        rightHand.GetComponent<LineRenderer>().enabled = !rightHand.GetComponent<LineRenderer>().enabled;
+        rightHand.GetComponent<XRInteractorLineVisual>().enabled = !rightHand.GetComponent<XRInteractorLineVisual>().enabled;
+        rightHand.GetComponent<XRRayInteractor>().enabled = !rightHand.GetComponent<XRRayInteractor>().enabled;
         rightHand.SetActive(!rightHand.activeSelf);
+        rightHandModel.SetActive(!rightHandModel.activeSelf);
+
+        if (bubble.activeSelf)
+        {
+            cylinder.GetComponent<CylinderSelector>().resetColliders();
+        }
+
         bubble.SetActive(!bubble.activeSelf);
+        bubbleModel.SetActive(!bubbleModel.activeSelf);
     }
+
     // Update is called once per frame
     void Update()
     {
